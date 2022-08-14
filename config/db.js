612ac0cd49027c78dc0e6e4eac9f344d7cb1c031
Aswin-S-T <aswinswebart15@gmail.com>
2022-08-14 16:05:55 +0530

    make it better

--- config/db.js
+++ config/db.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose')
+require('dotenv').config()
 const dbUrl = process.env.MONGO_URL || "mongodb://localhost:27017/my-socialmedia-application"
 
 module.exports.connect = ()=>{
+    console.log("DBURL------------", dbUrl);
     mongoose.connect(dbUrl,{
         useNewUrlParser : true,
         useUnifiedTopology : true
