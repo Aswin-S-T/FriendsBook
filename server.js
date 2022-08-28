63f0bbc12e2022805a4d651ca222e9b68b802f5a
Aswin-S-T <aswinswebart15@gmail.com>
2022-08-28 21:52:35 +0530

    new change

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ var corsOptions = {
 // Middlewares
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
-app.use(cors())
+app.use(corsOptions);
 // app.use(cors({ credentials: true }));
 
 
