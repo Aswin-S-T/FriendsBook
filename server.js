b0274cb2586de773df83a35a12014d69f4f08d61
Aswin-S-T <aswinswebart15@gmail.com>
2022-08-28 22:13:48 +0530

    dsds

--- server.js
+++ server.js
@@ -13,11 +13,12 @@ app.use((req, res, next) => {
   next();
 });
 
-// var corsOptions = {
-//   origin: "*",
-//   methods: ["GET", "POST","DELETE"],
-// };
-
+var corsOptions = {
+  origin: "*",
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  preflightContinue: false,
+  optionsSuccessStatus: 204,
+};
 // Middlewares
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
